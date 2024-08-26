af68e03a7329a660b2c74ce8f534e0c4de58cec2
shadowy-pycoder <shadowy-pycoder@example.com>
2024-08-26 20:48:31 +0300

    Small addings to the cli

--- cmd/mshark/cli.go
+++ cmd/mshark/cli.go
@@ -16,7 +16,9 @@ import (
 	"github.com/shadowy-pycoder/mshark/mpcapng"
 )
 
-const usagePrefix = `
+const app string = "mshark"
+
+const usagePrefix string = `
                 ______   __                            __       
                /      \ |  \                          |  \      
  ______ ____  |  $$$$$$\| $$____    ______    ______  | $$   __ 
@@ -37,6 +39,12 @@ Options:
 `
 
 var supportedFormats = []string{"txt", "pcap", "pcapng"}
+
+var (
+	_ ms.PacketWriter = &ms.Writer{}
+	_ ms.PacketWriter = &mpcap.Writer{}
+	_ ms.PacketWriter = &mpcapng.Writer{}
+)
 
 type ExtFlag []string
 
@@ -70,8 +78,8 @@ func displayInterfaces() error {
 	return w.Flush()
 }
 
-func createFile(ext string) (*os.File, error) {
-	path := fmt.Sprintf("./mshark_%s.%s", time.Now().UTC().Format("20060102_150405"), ext)
+func createFile(app, ext string) (*os.File, error) {
+	path := fmt.Sprintf("./%s_%s.%s", app, time.Now().UTC().Format("20060102_150405"), ext)
 	f, err := os.OpenFile(filepath.FromSlash(path), os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open file: %v", err)
@@ -82,7 +90,7 @@ func createFile(ext string) (*os.File, error) {
 func root(args []string) error {
 	conf := ms.Config{}
 
-	flags := flag.NewFlagSet("mshark", flag.ExitOnError)
+	flags := flag.NewFlagSet(app, flag.ExitOnError)
 	iface := flags.String("i", "any", "The name of the network interface. Example: eth0")
 	snaplen := flags.Int("s", 0, "The maximum length of each packet snapshot. Defaults to 65535.")
 	flags.BoolFunc("p", `Promiscuous mode. This setting is ignored for "any" interface. Defaults to false.`, func(flagValue string) error {
@@ -94,7 +102,7 @@ func root(args []string) error {
 	flags.StringVar(&conf.Expr, "e", "", `BPF filter expression. Example: "ip proto tcp"`)
 	flags.BoolFunc("D", "Display list of interfaces and exit.", func(flagValue string) error {
 		if err := displayInterfaces(); err != nil {
-			fmt.Fprintf(os.Stderr, "mshark: %v\n", err)
+			fmt.Fprintf(os.Stderr, "%s: %v\n", app, err)
 			os.Exit(2)
 		}
 		os.Exit(0)
@@ -131,7 +139,7 @@ func root(args []string) error {
 		for _, ext := range exts {
 			switch ext {
 			case "txt":
-				f, err := createFile(ext)
+				f, err := createFile(app, ext)
 				if err != nil {
 					return err
 				}
@@ -142,7 +150,7 @@ func root(args []string) error {
 				}
 				pw = append(pw, w)
 			case "pcap":
-				f, err := createFile(ext)
+				f, err := createFile(app, ext)
 				if err != nil {
 					return err
 				}
@@ -153,13 +161,13 @@ func root(args []string) error {
 				}
 				pw = append(pw, w)
 			case "pcapng":
-				f, err := createFile(ext)
+				f, err := createFile(app, ext)
 				if err != nil {
 					return err
 				}
 				defer f.Close()
 				w := mpcapng.NewWriter(f)
-				if err := w.WriteHeader("mshark", conf.Device, conf.Expr, conf.Snaplen); err != nil {
+				if err := w.WriteHeader(app, conf.Device, conf.Expr, conf.Snaplen); err != nil {
 					return err
 				}
 				pw = append(pw, w)
--- cmd/mshark/main.go
+++ cmd/mshark/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	if err := root(os.Args[1:]); err != nil {
-		fmt.Fprintf(os.Stderr, "mshark: %v\n", err)
+		fmt.Fprintf(os.Stderr, "%s: %v\n", app, err)
 		os.Exit(2)
 	}
 }
